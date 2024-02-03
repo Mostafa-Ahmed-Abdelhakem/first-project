6c53c0fcc5f98c202b75388694581a4d66886725
Mostafa Ahmed <mostafaabdelhakem7@gmail.com>
2024-02-03 14:59:50 +0200

    Notes website

---
+++ src/NoteList.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+const NoteList = ({ blogs }) => {
+  return (
+    <div className="blog-list">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>By: { blog.author }</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
+export default NoteList;
